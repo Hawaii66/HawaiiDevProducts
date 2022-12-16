9cb3b406df9540cee7dc61b3ea542ced19003570
Sebastian Ahlman <hawaiilive@outlook.com>
2022-12-17 00:36:56 +0100

    what2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx}",
-        "./src/components/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./Components/**/*.{js,ts,jsx,tsx}",
     ],
-    purge: [
-        "./src/components/**/*.{js,ts,jsx,tsx}",
-        "./src/pages/**/*.{js,ts,jsx,tsx}",
-    ],
+    purge: ["./Components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {},
     },
