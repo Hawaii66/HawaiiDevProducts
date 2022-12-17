d549452320e6a2af273be0fb870b53596f22007f
Sebastian Ahlman <hawaiilive@outlook.com>
2022-12-17 20:37:41 +0100

    color fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,5 +9,11 @@ module.exports = {
         extend: {},
     },
     plugins: [],
-    safelist: ["^bg-"],
+    safelist: [{
+            pattern: /^bg-/,
+        },
+        {
+            pattern: /^text-/,
+        },
+    ],
 };
