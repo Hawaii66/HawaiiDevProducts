12008709c2967d565cb5a2989d3d2a93d5b35f7e
Sebastian Ahlman <hawaiilive@outlook.com>
2022-12-17 18:16:19 +0100

    vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,22 @@ import { useEffect, useState } from "react";
 import HeadWrapper from "../Components/HeadWrapper";
 import Navbar from "../Components/Navbar";
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div
-      className="bg-gray-100 min-h-screen"
-      style={{
-        maxWidth: "100vw",
-      }}
-    >
-      <HeadWrapper />
-      <Navbar />
-      <Component {...pageProps} />
-    </div>
+    <>
+      <div
+        className="bg-gray-100 min-h-screen"
+        style={{
+          maxWidth: "100vw",
+        }}
+      >
+        <HeadWrapper />
+        <Navbar />
+        <Component {...pageProps} />
+      </div>
+      <Analytics />
+    </>
   );
 }
